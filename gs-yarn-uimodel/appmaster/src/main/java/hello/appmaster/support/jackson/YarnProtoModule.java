62deded7fc5d2666604e7d0b58946d2f695dec85
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-04-28 13:34:10 +0100

    updates

--- gs-yarn-uimodel/appmaster/src/main/java/hello/appmaster/support/jackson/YarnProtoModule.java
+++ gs-yarn-uimodel/appmaster/src/main/java/hello/appmaster/support/jackson/YarnProtoModule.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package hello.appmaster.support.jackson;
 
 import org.apache.hadoop.yarn.api.records.Container;
@@ -17,6 +32,9 @@ public class YarnProtoModule extends SimpleModule {
 
 	private static final long serialVersionUID = 3060130404108559404L;
 
+	/**
+	 * Instantiates a new yarn proto module.
+	 */
 	public YarnProtoModule() {
 		addSerializer(Container.class, new ContainerSerializer());
 		addSerializer(ContainerId.class, new ContainerIdSerializer());
