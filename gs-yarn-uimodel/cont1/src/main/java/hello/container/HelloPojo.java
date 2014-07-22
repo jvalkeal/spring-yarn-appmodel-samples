9b859ef3f2289d7689c02edbe1d0fce904383522
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-07-22 17:40:43 +0100

    updates

---
+++ gs-yarn-uimodel/cont1/src/main/java/hello/container/HelloPojo.java
@@ -0,0 +1,48 @@
+package hello.container;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.yarn.annotation.OnContainerStart;
+import org.springframework.yarn.annotation.YarnComponent;
+
+@YarnComponent
+public class HelloPojo {
+
+	private static final Log log = LogFactory.getLog(HelloPojo.class);
+
+	@Autowired
+	private Configuration configuration;
+
+	@OnContainerStart
+	public void publicVoidNoArgsMethod() throws Exception {
+		log.info("Hello from HelloPojo1.. sleeping");
+		try {
+			Thread.sleep(1000*60*30);
+		} catch (Exception e) {
+			log.info("Hello from HelloPojo1.. interrupted");
+		}
+		log.info("Hello from HelloPojo1.. sleep done");
+
+//		log.info("XXX1 topology.script.file.name=" + configuration.get("topology.script.file.name"));
+//		log.info("XXX2 net.topology.script.file.name=" + configuration.get("net.topology.script.file.name"));
+//
+//		Configuration cc = new Configuration(true);
+//		log.info("XXX1 topology.script.file.name=" + cc.get("topology.script.file.name"));
+//		log.info("XXX2 net.topology.script.file.name=" + cc.get("net.topology.script.file.name"));
+
+//		log.info("About to list from hdfs root content");
+
+//		@SuppressWarnings("resource")
+//		FsShell shell = new FsShell(configuration);
+//		for (FileStatus s : shell.ls(false, "/")) {
+//			log.info(s);
+//		}
+
+//		log.info("Sleep 60 sec");
+//		Thread.sleep(60000);
+//		log.info("Sleep done, bye");
+	}
+
+}
--- gs-yarn-uimodel/cont2/src/main/java/hello/container/HelloPojo.java
+++ gs-yarn-uimodel/cont2/src/main/java/hello/container/HelloPojo.java
@@ -17,13 +17,13 @@ public class HelloPojo {
 
 	@OnContainerStart
 	public void publicVoidNoArgsMethod() throws Exception {
-		log.info("Hello from HelloPojo.. sleeping");
+		log.info("Hello from HelloPojo2.. sleeping");
 		try {
 			Thread.sleep(1000*60*30);
 		} catch (Exception e) {
-			log.info("Hello from HelloPojo.. interrupted");
+			log.info("Hello from HelloPojo2.. interrupted");
 		}
-		log.info("Hello from HelloPojo.. sleep done");
+		log.info("Hello from HelloPojo2.. sleep done");
 
 //		log.info("XXX1 topology.script.file.name=" + configuration.get("topology.script.file.name"));
 //		log.info("XXX2 net.topology.script.file.name=" + configuration.get("net.topology.script.file.name"));
