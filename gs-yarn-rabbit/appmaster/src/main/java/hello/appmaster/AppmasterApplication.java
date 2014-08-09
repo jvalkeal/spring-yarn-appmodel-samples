6d678b3caa5c3d96710f1b8aca79e20e62ce2796
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-08-09 09:38:17 +0100

    updates

---
+++ gs-yarn-rabbit/appmaster/src/main/java/hello/appmaster/AppmasterApplication.java
@@ -0,0 +1,15 @@
+package hello.appmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class AppmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmasterApplication.class, args);
+	}
+
+}
