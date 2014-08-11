6fe6add360177610bb333501a7889ced6ff42052
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-08-11 09:18:05 +0100

    updates

--- gs-yarn-rabbit/appmaster/src/main/java/hello/appmaster/AppmasterApplication.java
+++ gs-yarn-rabbit/appmaster/src/main/java/hello/appmaster/AppmasterApplication.java
@@ -2,8 +2,10 @@ package hello.appmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@ComponentScan
 @Configuration
 @EnableAutoConfiguration
 public class AppmasterApplication {
