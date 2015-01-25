9fe9c042cf810715cf9128775df0c383cdfd31b7
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-01-25 18:47:43 +0000

    add sc-yarn-cloud

---
+++ sc-yarn-cloud/sc-yarn-admin/src/main/java/demo/AdminContainerApplication.java
@@ -0,0 +1,137 @@
+package demo;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.cluster.leader.event.AbstractLeaderEvent;
+import org.springframework.cloud.cluster.leader.event.LoggingListener;
+import org.springframework.cloud.cluster.leader.event.OnGrantedEvent;
+import org.springframework.cloud.cluster.leader.event.OnRevokedEvent;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SettableListenableFuture;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.yarn.annotation.OnContainerStart;
+import org.springframework.yarn.annotation.YarnComponent;
+import org.springframework.yarn.container.YarnContainerSupport;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class AdminContainerApplication {
+
+	private static final Log log = LogFactory.getLog(AdminContainerApplication.class);
+	
+	@Autowired
+    private Configuration configuration;
+	
+	@Bean
+	public LoggingListener loggingListener() {
+		return new LoggingListener("info");
+	}
+	
+	@YarnComponent
+	public static class StoreContainerWriter extends YarnContainerSupport
+			implements ApplicationListener<AbstractLeaderEvent> {
+		
+		private AtomicBoolean active = new AtomicBoolean();
+		
+//		@Autowired
+//		private RestTemplate restTemplate;
+		
+		@Autowired
+		private StoreIntegration storeIntegration;
+		
+		@OnContainerStart
+		public ListenableFuture<?> writer() throws Exception {
+			final WriterFuture future = new WriterFuture();
+
+			getTaskScheduler().schedule(new FutureTask<Void>(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						while (!future.interrupted) {
+							if (active.get()) {
+								String entity = "Time is " + System.currentTimeMillis();
+								try {
+									String response = storeIntegration.writeEntity(entity);
+//									String response = restTemplate.postForObject(
+//											"http://storecontainer/store", entity,
+//											String.class);
+									log.info("response from storecontainer is: " + response);								
+								} catch (Exception e) {
+//									log.warn("Error in restTemplate", e);
+									log.warn("Error in hystrix", e);
+								}
+							} else {
+								log.info("Not a leader");
+							}
+							Thread.sleep(1000);
+						}
+					} catch (Exception e) {
+//						future.set(false);
+						log.error("Error in writing", e);
+					}
+				}
+			}, null), new Date());
+
+			return future;
+		}
+
+		@Override
+		public void onApplicationEvent(AbstractLeaderEvent event) {
+			if (event instanceof OnGrantedEvent) {
+				active.set(true);
+			} else if (event instanceof OnRevokedEvent){
+				active.set(false);
+			}
+		}
+		
+	}
+	
+	@Component
+	static class StoreIntegration {
+
+		@Autowired
+		private RestTemplate restTemplate;
+		
+		@HystrixCommand(fallbackMethod = "defaultWriteEntity")
+		public String writeEntity(String entity) {
+			restTemplate.postForObject("http://storecontainer/store", entity, String.class);
+			return "ok";
+		}
+
+		public String defaultWriteEntity(String entity) {
+			return "error";
+		}
+	}
+	
+	static class WriterFuture extends SettableListenableFuture<Boolean> {
+
+		boolean interrupted = false;
+
+		@Override
+		protected void interruptTask() {
+			interrupted = true;
+		}
+	}	
+		
+	public static void main(String[] args) {
+		SpringApplication.run(AdminContainerApplication.class, args);
+	}
+
+}
